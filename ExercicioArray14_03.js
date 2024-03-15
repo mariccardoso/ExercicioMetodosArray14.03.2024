5abc982678b893c5c16b215be9ae2ee18a133d26
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-03-15 16:46:10 -0300

    Método 03: .slice()

--- ExercicioArray14_03.js
+++ ExercicioArray14_03.js
@@ -17,6 +17,9 @@ console.log("Testes práticos: .concat")
 let array3 = array1.concat(array2);
 console.log(array3);
 
+let array4 = array1.concat(array2, array3);
+console.log(array4);
+
 
 //Metodo 02: .join()
 //O método join() é usado para criar uma string a partir dos elementos de um array.
@@ -35,3 +38,20 @@ console.log(equipe.join());
 
 //Separar os elementos da seguinte forma: Júlia-Vinicius-Fernanada-Rayssa-Mariana
 console.log(equipe.join('-'))
+
+
+//Metodo 03: .slice()
+//O método slice() é usado para retornar uma cópia de parte de um array, especificada pelos índices de início e fim.
+
+//Sintaxe:
+//slice(start)
+//slice(start, end)
+
+//Testes Práticos: 
+console.log("Testes práticos: .slice()")
+
+//Resultado esperado: Array ['Vinicius', 'Fernanda', 'Rayssa', 'Mariana']
+console.log(equipe.slice(1));
+
+//Resultado esperado: Array['a', 'b', 'c']
+console.log(array1.slice(0, 3));
